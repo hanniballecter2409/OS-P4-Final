366ab00c80c2e4f5b051ee95b2759b047943e6d6
Hannibal Lecter 2409 <sumukhakollegal@gmail.com>
2024-11-26 17:46:09 -0800

    FINAL

--- logfs.c
+++ logfs.c
@@ -37,8 +37,8 @@ struct logfs {
     Thread Specific Variables
     */
     pthread_t thread;
-    uint64_t child_allow;
-    uint64_t par_allow;
+    uint64_t write_allow;
+    uint64_t read_allow;
     uint64_t should_exit;
     pthread_mutex_t mutex;
     pthread_mutex_t print;
@@ -89,8 +89,8 @@ struct logfs *logfs_open(const char *pathname) {
     Thread specific variables
     */
     fs->should_exit = 0;
-    fs->child_allow = 0;
-    fs->par_allow = 0;
+    fs->write_allow = 0;
+    fs->read_allow = 0;
     fs->done = 0;
 
     if (pthread_mutex_init(&(fs->mutex), NULL) != 0) {
@@ -114,7 +114,7 @@ struct logfs *logfs_open(const char *pathname) {
 void logfs_close(struct logfs *fs) {
     if (fs) {
         fs->should_exit = 1;
-        fs->child_allow = 1;
+        fs->write_allow = 1;
         pthread_cond_signal(&fs->can_write);
         pthread_join(fs->thread, NULL);
         pthread_mutex_destroy(&fs->mutex);
@@ -188,7 +188,7 @@ int logfs_append(struct logfs *fs, const void *buf, uint64_t len) {
     if(fs->write_head + len <= fs->write_limit){
         memcpy((void*)fs->write_head, buf, len);
         fs->write_head += len;
-        fs->child_allow = 1; /* Can start flushing from write buffer to device*/
+        fs->write_allow = 1; /* Can start flushing from write buffer to device*/
         pthread_cond_signal(&fs->can_write);
         return 0;
     }
@@ -198,20 +198,20 @@ int logfs_append(struct logfs *fs, const void *buf, uint64_t len) {
     len -= free_space;
     memcpy((void*)fs->write_queue, buf+free_space , len);
     fs->write_head = (uint64_t)fs->write_queue + len;
-    fs->child_allow = 1; /* Can start flushing from write buffer to device*/
+    fs->write_allow = 1; /* Can start flushing from write buffer to device*/
     pthread_cond_signal(&fs->can_write); /* Send signal to writer that data is available to flush */
     return 0;
 }
 
 
-int logfs_flush_(struct logfs *fs, uint64_t from, uint64_t to){
+int logfs_flush_(struct logfs *fs, uint64_t start, uint64_t end){
     uint64_t diff;
     void * temp_mem;
     uint64_t diff_;
     uint64_t len;
     uint64_t len_diff;
 
-    if( from == to)
+    if( start == end)
     {
         return 0;
     }
@@ -223,38 +223,38 @@ int logfs_flush_(struct logfs *fs, uint64_t from, uint64_t to){
       memset(temp_mem, 0, fs->block_size);
 
       device_read(fs->device, temp_mem, (fs->device_limit - diff),fs->block_size);
-      memcpy((void *)(temp_mem+diff),(void *)from, diff_);
+      memcpy((void *)(temp_mem+diff),(void *)start, diff_);
       device_write(fs->device, temp_mem, (fs->device_limit - diff),fs->block_size);
       FREE(temp_mem);
-      if((from + diff_)>to)
+      if((start + diff_)>end)
       {
-        fs->device_limit += to - from;
+        fs->device_limit += end - start;
         return 0;
       }else{
         fs->device_limit += diff_;
-        from += diff_;                                                    
+        start += diff_;                                                    
       }
 
     }
-    if((from % fs->block_size)>0 )
+    if((start % fs->block_size)>0 )
     {
         temp_mem = aligned_alloc(fs->block_size,fs->block_size * WCACHE_BLOCKS);
         memset(temp_mem, 0, fs->block_size * WCACHE_BLOCKS);
-        len = to - from;
-        memcpy(temp_mem,(void *)from, len);
+        len = end - start;
+        memcpy(temp_mem,(void *)start, len);
         logfs_flush_(fs, (uint64_t) temp_mem, (uint64_t)temp_mem + len);
         FREE(temp_mem);
         return 0;
 
     }
-    len = to-from;
+    len = end-start;
     len_diff = len%fs->block_size;
     if(len_diff>0)
     {
         len += fs->block_size-len_diff;
     }
-    device_write(fs->device, (void*)from, fs->device_limit, len);
-    fs->device_limit += to - from;
+    device_write(fs->device, (void*)start, fs->device_limit, len);
+    fs->device_limit += end - start;
     return 0;
 
 }
@@ -280,12 +280,12 @@ void *writer_function(void *arg) {
     while (!fs->should_exit) {
         pthread_mutex_lock(&fs->mutex);
         /* Program should not exit and Child Process not allowed i.e. Read not allowed then we can write */
-        while ((!fs->should_exit) && !fs->child_allow) { 
+        while ((!fs->should_exit) && !fs->write_allow) { 
             pthread_cond_wait(&fs->can_write, &fs->mutex);
         }
         logfs_flush(fs);
-        fs->child_allow = 0;
-        fs->par_allow = 1;
+        fs->write_allow = 0;
+        fs->read_allow = 1;
         pthread_cond_signal(&fs->can_read);
         pthread_mutex_unlock(&fs->mutex);
     }
@@ -296,11 +296,11 @@ void *reader_function(struct logfs *fs) {
 
         pthread_mutex_lock(&fs->mutex);
 
-        fs->child_allow = 1;
-        fs->par_allow = 0;
+        fs->write_allow = 1;
+        fs->read_allow = 0;
         pthread_cond_signal(&fs->can_write);
 
-        while(!fs->par_allow){
+        while(!fs->read_allow){
             pthread_cond_wait(&fs->can_read, &fs->mutex);
         };
         pthread_mutex_unlock(&fs->mutex);
